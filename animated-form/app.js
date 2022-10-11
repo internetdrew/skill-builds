2e5bae2ec5eb57a7edcfbbb33f6314c98989ed28
Andrew Rowley <andrew@internetdrew.com>
2022-10-11 10:56:37 -0400

    Add visual input validations

--- animated-form/app.js
+++ animated-form/app.js
@@ -2,12 +2,35 @@
 
 const arrows = document.querySelectorAll('.fa-arrow-down');
 const form = document.querySelector('form');
+const username = document.getElementById('username');
 
-form.addEventListener('click', e => {
-  const field = e.target.closest('.field');
+const showStatus = function (status) {
+  if (status === 'error') {
+    document.body.style.backgroundColor = 'rgb(189, 87, 87)';
+  }
+  if (status === 'success') {
+    document.body.style.backgroundColor = 'rgb(87, 189, 130)';
+  }
+};
+
+const validateUsername = function () {
+  const valid = username.value.length > 6;
+  console.log(valid);
+
+  if (!valid) {
+    console.log('Not enough characters');
+    showStatus('error');
+  }
+
+  if (valid) showStatus('success');
+};
+
+// Event listeners
+form.addEventListener('click', () => {
+  const field = form.querySelector('.field');
   const arrow = field.querySelector('.fa-arrow-down');
   const input = field.querySelector('input');
   const nextField = field.nextElementSibling;
 
-  console.log(field, input, arrow, nextField);
+  console.log(field, arrow, nextField);
 });
