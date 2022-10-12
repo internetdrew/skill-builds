0b89d6807a39070899670cee6ca3631712bc21a6
Andrew Rowley <andrew@internetdrew.com>
2022-10-12 12:53:22 -0400

    Change weather api for unlimited calls

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,22 @@
 'strict mode';
-const API_KEY = config.API_KEY;
 
-const getLocationKey = async function (position) {
-  const { latitude, longitude } = position.coords;
-
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const api = `${proxy}http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${API_KEY}&q=${latitude}%2C${longitude}`;
-
-  const res = await fetch(api);
-  const data = await res.json();
-  return data.Key;
-};
+const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 window.addEventListener('load', () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async position => {
-      const locationKey = await getLocationKey(position);
-      console.log(locationKey);
+      const { latitude, longitude } = position.coords;
+      const api = `https://api.weather.gov/points/${latitude},${longitude}`;
+
+      const res = await fetch(api);
+      const data = await res.json();
+      console.log(data);
+      const { city, state } = data.properties.relativeLocation.properties;
+      console.log(city, state);
+
+      const forecastRes = await fetch(data.properties.forecast);
+      const forecastData = await forecastRes.json();
+      console.log(forecastData);
     });
   }
 });
