3cbbddb771e9ab888babd5a5900c5062b86fccb6
Andrew Rowley <andrew@internetdrew.com>
2022-10-13 11:18:50 -0400

    Consolidate forecast data into object

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,56 @@
 'strict mode';
 
-const locationName = document.getElementById('location-name');
+const locationNameEl = document.getElementById('location-name');
 
 const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+const getLocationForecast = async function (lat, long) {
+  const api = `https://api.weather.gov/points/${lat},${long}`;
+  const res = await fetch(api);
+  if (res.ok) console.log('success: 1');
+  const data = await res.json();
+  const { city, state } = data.properties.relativeLocation.properties;
+  const { forecast } = data.properties;
+  return { city, state, forecast };
+};
+
+const displayForecast = function (forecastObj) {};
 
 window.addEventListener('load', () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async position => {
-      const { latitude, longitude } = position.coords;
-      const api = `https://api.weather.gov/points/${latitude},${longitude}`;
+      const { latitude: lat, longitude: long } = position.coords;
+      const { city, state, forecast } = await getLocationForecast(lat, long);
+      locationNameEl.textContent = `${city}, ${state}`;
 
-      const res = await fetch(api);
+      const res = await fetch(forecast);
+      if (res.ok) console.log('success: 2');
       const data = await res.json();
-      console.log(data);
-      const { city, state } = data.properties.relativeLocation.properties;
-      console.log(city, state);
-      locationName.textContent = `${city}, ${state}`;
+      console.log(data.properties.periods[0]);
 
-      const forecastRes = await fetch(data.properties.forecast);
-      const forecastData = await forecastRes.json();
-      console.log(forecastData);
+      const {
+        name: time,
+        temperature,
+        temperatureUnit,
+        detailedForecast,
+        isDaytime,
+        icon,
+        shortForecast,
+      } = data.properties.periods[0];
+
+      const forecastData = {
+        city,
+        state,
+        time,
+        temperature,
+        temperatureUnit,
+        detailedForecast,
+        isDaytime,
+        icon,
+        shortForecast,
+      };
+
+      displayForecast(forecastData);
     });
   }
 });
