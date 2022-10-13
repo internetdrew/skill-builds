34d37a048682a7ff961b9ff37645118297e2989d
Andrew Rowley <andrew@internetdrew.com>
2022-10-13 12:25:54 -0400

    Display data on load

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,32 +1,45 @@
 'strict mode';
 
 const locationNameEl = document.getElementById('location-name');
+const timeEl = document.getElementById('time');
+const iconEl = document.getElementById('icon');
+const temperatureEl = document.getElementById('temperature');
+const temperatureUnitEl = document.getElementById('temperature-unit');
+const tempDescriptionEl = document.getElementById('temperature-description');
 
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 const getLocationForecast = async function (lat, long) {
   const api = `https://api.weather.gov/points/${lat},${long}`;
+
   const res = await fetch(api);
-  if (res.ok) console.log('success: 1');
+  if (!res.ok) console.log('Problem fetching forecast: 1');
+
   const data = await res.json();
-  const { city, state } = data.properties.relativeLocation.properties;
   const { forecast } = data.properties;
+  const { city, state } = data.properties.relativeLocation.properties;
   return { city, state, forecast };
 };
 
-const displayForecast = function (forecastObj) {};
+const displayForecast = function (forecastObj) {
+  console.log(forecastObj);
+  locationNameEl.textContent = `${forecastObj.city}, ${forecastObj.state}`;
+
+  timeEl.textContent = forecastObj.time;
+  temperatureEl.textContent = forecastObj.temperature;
+  temperatureUnitEl.textContent = forecastObj.temperatureUnit;
+  tempDescriptionEl.textContent = forecastObj.shortForecast;
+};
 
 window.addEventListener('load', () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async position => {
       const { latitude: lat, longitude: long } = position.coords;
       const { city, state, forecast } = await getLocationForecast(lat, long);
-      locationNameEl.textContent = `${city}, ${state}`;
 
       const res = await fetch(forecast);
-      if (res.ok) console.log('success: 2');
+      if (!res.ok) console.log('Problem fetching forecast: 2');
       const data = await res.json();
-      console.log(data.properties.periods[0]);
 
       const {
         name: time,
