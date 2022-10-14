baacb5ac79dab094fdce8594ac8b3870ad3987b7
Andrew Rowley <andrew@internetdrew.com>
2022-10-13 23:33:01 -0400

    Add continuous scrolling motion both directions

--- carousel/app.js
+++ carousel/app.js
@@ -12,18 +12,33 @@ const imgWidth = carouselImages[0].clientWidth;
 carouselSlider.style.transform = `translateX(-${imgWidth}px)`;
 
 nextBtn.addEventListener('click', () => {
-  if (currentSlide < carouselImages.length - 1) {
-    currentSlide++;
-    carouselSlider.style.transform = `translateX(-${
-      imgWidth * currentSlide
-    }px)`;
-    console.log(currentSlide, carouselSlider);
-  }
+  carouselSlider.style.transition = 'transform 0.3s ease-in-out';
+  currentSlide++;
+  carouselSlider.style.transform = `translateX(-${imgWidth * currentSlide}px)`;
 });
 
-// prevBtn.addEventListener('click', () => {
-//   if (currentSlide > -1) {
-//     currentSlide--;
-//     carousel.style.transform = ``;
-//   }
-// });
+prevBtn.addEventListener('click', () => {
+  carouselSlider.style.transition = 'transform 0.3s ease-in-out';
+  currentSlide--;
+  carouselSlider.style.transform = `translateX(${-imgWidth * currentSlide}px)`;
+});
+
+carouselSlider.addEventListener('transitionend', () => {
+  if (carouselImages[currentSlide].id === 'last-clone') {
+    carouselSlider.style.transition = 'none';
+    currentSlide = carouselImages.length - 2;
+    carouselSlider.style.transform = `translateX(${
+      -imgWidth * currentSlide
+    }px)`;
+  }
+
+  if (carouselImages[currentSlide].id === 'first-clone') {
+    carouselSlider.style.transition = 'none';
+    console.log(currentSlide);
+    currentSlide = carouselImages.length - currentSlide;
+    console.log(currentSlide);
+    carouselSlider.style.transform = `translateX(${
+      -imgWidth * currentSlide
+    }px)`;
+  }
+});
