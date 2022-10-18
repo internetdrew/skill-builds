871bd178aa98231da3fde64e50d2a92e3cb8189b
Andrew Rowley <andrew@internetdrew.com>
2022-10-18 15:03:30 -0400

    Change visit button to styled link

--- bookmarker/app.js
+++ bookmarker/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const form = document.getElementById('form');
+const siteNameEl = docuement.getElementById('site-name');
+const siteUrlEl = docuement.getElementById('site-url');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
