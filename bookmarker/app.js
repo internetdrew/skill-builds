54210445fc4db14a3bb4779a827b52f946a6dfa1
Andrew Rowley <andrew@internetdrew.com>
2022-10-18 22:02:01 -0400

    Add functionality to save bookmarks to local storage

--- bookmarker/app.js
+++ bookmarker/app.js
@@ -1,9 +1,31 @@
 'use strict';
 
 const form = document.getElementById('form');
-const siteNameEl = docuement.getElementById('site-name');
-const siteUrlEl = docuement.getElementById('site-url');
+const siteNameEl = document.getElementById('site-name');
+const siteUrlEl = document.getElementById('site-url');
 
-form.addEventListener('submit', e => {
+const saveBookmark = function (e) {
   e.preventDefault();
-});
+
+  const siteName = siteNameEl.value;
+  const siteUrl = siteUrlEl.value;
+
+  const bookmark = {
+    name: siteName,
+    url: siteUrl,
+  };
+
+  if (localStorage.getItem('bookmarks') === null) {
+    const bookmarks = [];
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
+  if (localStorage.getItem('bookmarks') !== null) {
+    const bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+};
+
+form.addEventListener('submit', saveBookmark);
