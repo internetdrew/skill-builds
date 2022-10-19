bc902d6451b51eb81b42e7090cf999b5d5fa10fd
Andrew Rowley <andrew@internetdrew.com>
2022-10-18 23:08:16 -0400

    Load saved bookmarks on window load event

--- bookmarker/app.js
+++ bookmarker/app.js
@@ -3,6 +3,35 @@
 const form = document.getElementById('form');
 const siteNameEl = document.getElementById('site-name');
 const siteUrlEl = document.getElementById('site-url');
+const bookmarksEl = document.getElementById('bookmarks');
+
+const clearChildEls = function (parentEl) {
+  while (parentEl.firstChild) {
+    parentEl.removeChild(parentEl.firstChild);
+  }
+};
+
+const clearValues = function (...elArray) {
+  elArray.forEach(el => (el.value = ''));
+};
+
+const renderBookmarks = function (bookmarks) {
+  bookmarks
+    .map(bookmark => {
+      const html = `
+       <div class="bookmark">
+          <span class="bookmark-name">${bookmark.name}</span>
+          <div class="bookmark-btns">
+            <a href="${bookmark.url}" class="bookmark--btn btn-visit">Visit</a>
+            <button class="bookmark--btn btn-delete">Delete</button>
+          </div>
+        </div>
+  `;
+
+      bookmarksEl.insertAdjacentHTML('beforeend', html);
+    })
+    .join();
+};
 
 const saveBookmark = function (e) {
   e.preventDefault();
@@ -27,21 +56,19 @@ const saveBookmark = function (e) {
     const bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     bookmarks.push(bookmark);
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    clearChildEls(bookmarksEl);
+    clearValues(siteNameEl, siteUrlEl);
+    renderBookmarks(bookmarks);
   }
 };
 
-const renderBookmarks = function (bookmarks) {
-  bookmarks
-    .map(bookmark => {
-      const markup = `
-       <div class="bookmark">
-          <span class="bookmark-name">Google</span>
-          <a href="#" class="bookmark--btn btn-visit">Visit</a>
-          <button class="bookmark--btn btn-delete">Delete</button>
-        </div>
-  `;
-    })
-    .join();
+const loadSavedBookmarks = function () {
+  if (localStorage.getItem('bookmarks') === null) return;
+
+  const bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  clearChildEls(bookmarksEl);
+  renderBookmarks(bookmarks);
 };
 
+window.addEventListener('load', loadSavedBookmarks);
 form.addEventListener('submit', saveBookmark);
