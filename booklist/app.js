94ca76dda1a4b8848c4a7111b5c93f40ca6b9eb2
Andrew Rowley <andrew@internetdrew.com>
2022-10-22 15:02:15 -0400

    Add books to local storage and display

--- booklist/app.js
+++ booklist/app.js
@@ -1,4 +1,5 @@
 'use strict';
+const form = document.querySelector('.form');
 
 class Book {
   constructor(title, author, isbn) {
@@ -10,22 +11,13 @@ class Book {
 
 class UI {
   static displayBooks() {
-    const StoredBooks = [
-      {
-        title: 'Book One',
-        author: 'John Doe',
-        isbn: '632836',
-      },
-      {
-        title: 'Book Two',
-        author: 'Jane Doe',
-        isbn: '028366',
-      },
-    ];
+    const storedBooks =
+      localStorage.getItem('books') === null
+        ? []
+        : JSON.parse(localStorage.getItem('books'));
 
-    const books = StoredBooks;
-
-    books.forEach(book => UI.addBookToList(book));
+    if (storedBooks.length > 0)
+      storedBooks.forEach(book => UI.addBookToList(book));
   }
 
   static addBookToList(book) {
@@ -38,9 +30,39 @@ class UI {
             <td class="delete-btn">X</td>
           </tr>
     `;
-
     table.insertAdjacentHTML('beforeend', html);
   }
 }
 
+class Storage {
+  static addBook(e) {
+    e.preventDefault();
+    const titleEl = document.getElementById('title');
+    const authorEl = document.getElementById('author');
+    const isbnEl = document.getElementById('isbn');
+
+    const inputs = [titleEl, authorEl];
+
+    if (inputs.some(input => input.value.trim() === '')) return;
+
+    const books =
+      localStorage.getItem('books') === null
+        ? []
+        : JSON.parse(localStorage.getItem('books'));
+
+    const book = {
+      title: titleEl.value.trim(),
+      author: authorEl.value.trim(),
+      isbn: isbnEl.value.trim(),
+    };
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+    UI.displayBooks();
+    form.reset();
+  }
+}
+
 window.addEventListener('load', UI.displayBooks);
+form.addEventListener('submit', Storage.addBook);
